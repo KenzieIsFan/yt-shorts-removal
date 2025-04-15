7f06d679878e7d77794babef5e53fd4eafd77aee
KenzieIsFan <kenzie.fauzi@gmail.com>
2025-04-15 20:48:01 +0100

    Added file structure and basic code for extension to work

--- content.js
+++ content.js
@@ -1,1 +1,18 @@
 
+//MANIFEST: content script ties directly to 
+const short_limit = 3;
+
+var viewed = 0;
+var currentURL = window.location.href
+
+//popstate doesnt work with shorts because the history stack does not change
+window.setInterval(function(){
+    if (currentURL != window.location.href){
+        viewed = viewed + 1;
+        currentURL = window.location.href
+        if (viewed >= short_limit){
+            //sends you to homepage
+            window.location.href = "www.youtube.com"
+        }
+    } 
+},250)
